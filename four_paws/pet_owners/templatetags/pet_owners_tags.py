8155a7afd7b53c1c4644e4b99d577eba29943e5c
Dmitriy <lion95-10@mail.ru>
2023-10-01 21:39:49 +0300

    moved templatetags that relate to posts to the posts app

--- four_paws/pet_owners/templatetags/pet_owners_tags.py
+++ four_paws/pet_owners/templatetags/pet_owners_tags.py
@@ -11,12 +11,6 @@ def create_foll_list(animal_name, user_animals_followed):
 
 
 @register.simple_tag
-def get_data_for_post(post_title, post_data):
-    """достает данные из словаря для нужного поста"""
-    return post_data[post_title]
-
-
-@register.simple_tag
 def create_range(start, finish):
     """Решает проблему с точкой для range в шаблоне"""
     return range(start, finish)
@@ -30,21 +24,9 @@ def get_object_on_index(array, index):
         return None
 
 
-@register.inclusion_tag('pet_owners/likes_and_views_for_posts.html')
-def show_likes_and_views(post, post_data, name_page_for_likes, object_id):
-    return {"post": post, "post_data": post_data,
-            "name_page_for_likes": name_page_for_likes, "object_id": object_id}
-
-
 @register.inclusion_tag('pet_owners/form_fields.html')
 def show_form_fields(form):
     return {'form': form}
-
-
-@register.inclusion_tag('pet_owners/all_posts.html')
-def show_all_posts(posts, data_for_post, name_page_for_likes, object_id):
-    return {"posts": posts, "data_for_post": data_for_post,
-            "name_page_for_likes": name_page_for_likes, "object_id": object_id}
 
 
 @register.simple_tag
---
+++ four_paws/posts/templatetags/posts_tags.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('posts/all_posts.html')
+def show_all_posts(posts, data_for_post, name_page_for_likes, object_id):
+    return {"posts": posts, "data_for_post": data_for_post,
+            "name_page_for_likes": name_page_for_likes, "object_id": object_id}
+
+
+@register.simple_tag
+def get_data_for_post(post_pk, post_data):
+    """достает данные из словаря для нужного поста"""
+    return post_data[post_pk]
+
+
+@register.inclusion_tag('posts/likes_and_views_for_posts.html')
+def show_likes_and_views(post, post_data, name_page_for_likes, object_id):
+    return {"post": post, "post_data": post_data,
+            "name_page_for_likes": name_page_for_likes, "object_id": object_id}
